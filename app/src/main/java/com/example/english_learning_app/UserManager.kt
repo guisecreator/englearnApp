836e0f45886a1a39203d7fde588e4bdc583e61ce
Nikita Semyachkin <93248541+guisecreator@users.noreply.github.com>
2023-05-23 14:50:09 +0500

    feat: added styles for buttons

---
+++ app/src/main/java/com/example/english_learning_app/UserManager.kt
@@ -0,0 +1,35 @@
+package com.example.english_learning_app
+
+import android.content.Context
+
+class UserManager(context: Context) {
+    companion object {
+        private const val PREFS_NAME = "UserPrefs"
+        private const val KEY_USERNAME = "username"
+        private const val KEY_PASSWORD = "password"
+    }
+
+    private val sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+    fun saveCredentials(username: String, password: String) {
+        sharedPreferences.edit().apply {
+            putString(KEY_USERNAME, username)
+            putString(KEY_PASSWORD, password)
+            apply()
+        }
+    }
+
+    fun getRegisteredUsername(): String {
+        return sharedPreferences.getString(KEY_USERNAME, "") ?: ""
+    }
+
+    fun getRegisteredPassword(): String {
+        return sharedPreferences.getString(KEY_PASSWORD, "") ?: ""
+    }
+
+    fun checkCredentials(username: String, password: String): Boolean {
+        val registeredUsername = getRegisteredUsername()
+        val registeredPassword = getRegisteredPassword()
+        return username == registeredUsername && password == registeredPassword
+    }
+}
