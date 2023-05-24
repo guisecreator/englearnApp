918ed308fcff778d2b9f3ed6b0fd6970e37cca48
Nikita Semyachkin <93248541+guisecreator@users.noreply.github.com>
2023-05-24 23:05:11 +0500

    feat: added questions

--- app/src/main/java/com/example/english_learning_app/LessonOKActivity.kt
+++ app/src/main/java/com/example/english_learning_app/LessonOKActivity.kt
@@ -11,15 +11,39 @@ class QuizActivity : AppCompatActivity() {
     private var currentQuestionIndex: Int = 0
 
     private val questions = listOf(
-        "Вопрос 1",
-        "Вопрос 2",
-        "Вопрос 3"
+        "What’s ... name?",
+        "Where ... from?",
+        "In my house ... only two bedrooms.",
+        "She ... blue eyes.",
+        "My father ... in an office.",
+        "- Where’s Dad?\n" + "- ... TV in the living room.\n",
+        "Last weekend we ...",
+        "Is there ... interesting information on that website?",
+        "Elephants are ... than lions.",
+        "We are going ... to London.",
+        "Don’t worry. You... try to relax.",
+        "What ... yesterday at 11 p.m.?",
+        "Have you ... the president?",
+        "Will life ... different in 100 years?",
+        "Tomorrow I ... with my friends.",
     )
 
     private val answers = listOf(
-        listOf("Вариант ответа 1", "Вариант ответа 2", "Вариант ответа 3"),
-        listOf("Вариант ответа 1", "Вариант ответа 2", "Вариант ответа 3"),
-        listOf("Вариант ответа 1", "Вариант ответа 2", "Вариант ответа 3")
+        listOf("he", "your", "she"),
+        listOf("are they", "is they", "there are a"),
+        listOf("have got", "has got", "got"),
+        listOf("work", "works", "working"),
+        listOf("He’s watching", "He watches", "He watching"),
+        listOf("listen to music", "listened to music", "did listened to music"),
+        listOf("any", "some", "an"),
+        listOf("heavier", "more heavier", "heavy"),
+        listOf("flying", "fly", "to fly"),
+        listOf("should", "should to", "shouldn’t"),
+        listOf("did you doing", "were you doing", "you were doing"),
+        listOf("ever met", "ever meet", "meet"),
+        listOf("be", "is", "to be"),
+        listOf("am playing", "will play", "play"),
+        listOf("won’t", "don’t", "are not"),
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,8 +89,6 @@ class QuizActivity : AppCompatActivity() {
         } else {
             val intent = Intent(this@QuizActivity, ResultActivity::class.java)
             intent.putExtra("score", score)
-            intent.putExtra("total", score)
-
             // Все вопросы пройдены, переход к экрану с результатами
             intent.putExtra("totalQuestions", questions.size)
             startActivity(intent)
