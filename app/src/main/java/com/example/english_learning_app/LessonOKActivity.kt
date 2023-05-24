075937f4b9ac90eada25d4e2df4ecc691e36a3c4
Nikita Semyachkin <93248541+guisecreator@users.noreply.github.com>
2023-05-24 18:56:37 +0500

    feat: added activity_lessons xml

--- app/src/main/java/com/example/english_learning_app/LessonOKActivity.kt
+++ app/src/main/java/com/example/english_learning_app/LessonOKActivity.kt
@@ -3,7 +3,6 @@ package com.example.english_learning_app
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_lesson.*
 import kotlinx.android.synthetic.main.activity_lesson_ok.*
 
 class QuizActivity : AppCompatActivity() {
